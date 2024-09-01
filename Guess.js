cfb060f4fe2152f5e989194947ec2fc8aec57af2
Azhar ali <162332161+Azharbalouch456@users.noreply.github.com>
2024-09-01 17:22:15 +0500

    Add files via upload

---
+++ Guess.js
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+// 1) The computer will produce a random number.
+// 2) The user will guess the number.
+// 3) Match the user input with the randomly generated number.
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "pleas predict the number within the range of 1 to 10"
+    }
+]);
+if (answer.userGuessNumber == randomNumber) {
+    console.log("Congrulationss ! You are guess right number");
+}
+else {
+    console.log("sorry you Guess wrong number");
+}
